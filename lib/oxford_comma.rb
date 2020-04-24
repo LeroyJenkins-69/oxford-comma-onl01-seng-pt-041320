e62e5643e834bcb7eed48c894842c0e5ee31db9e
Kordell Parrish <parrishkordell@gmail.com>
2020-04-24 19:46:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
- array.join(", ")
+  if array.length == 1 
+    array[0]
+  elsif array.length == 2 
+  array [1]
+  array.join("and ")
+  else
+    array[array.length-1 ] = "and" << array[array.length-1 ]
+    array.join(", ")
 end
